523f496ce484ef3d809710d84dc6f335bf46a052
starlord <starlord@xhiftcorp.com>
2019-05-14 11:11:19 +0900

    withLayout

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+import withLayout from "../lib/withLayout";
+
+console.log("about");
+const about = () => (
+  <div>
+    <Head>
+      <title>About | Nomad Store</title>
+    </Head>
+    <h1>About</h1>
+  </div>
+);
+
+export default withLayout(about);
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
-import React from "react"
-console.log("hello")
-export default () => < h1 > Hello index test < /h1>;
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import withLayout from "../lib/withLayout";
+
+console.log("hello");
+const Index = () => (
+  <div>
+    <Head>
+      <title>Index | Nomad Store</title>
+    </Head>
+    <h1> Hello index test </h1>
+    {/* <Link href="/about">
+      <a> About </a>
+    </Link> */}
+  </div>
+);
+
+export default withLayout(Index);
